2b682c402e000f889590ca785abca26992e1b790
Michael Neises <m811n155@n285.local>
2023-07-01 13:58:04 -0500

    fix for singularity; unwrap from docker

--- build_app.sh
+++ build_app.sh
@@ -10,12 +10,9 @@ fi
 # Get the version from the command line argument
 version=$1
 
-THIS_DIR=`pwd`
-cd seL4-CAmkES-L4v-dockerfiles &&
-make EXEC='bash -c "\
-    rm -rf /host/build-'$version' || { echo '\''Failed to delete /host/build-'$version''\''; exit 1; }; \
-    mkdir /host/build-'$version' || { echo '\''Failed to create /host/build-'$version''\''; exit 1; }; \
-    cd /host/build-'$version' || { echo '\''Failed to change to /host/build-'$version' directory'\''; exit 1; }; \
-    ../init-build.sh -DCAMKES_VM_APP=attarch-'$version' -DPLATFORM=qemu-arm-virt && \
-    ninja"' user HOST_DIR=$THIS_DIR
+rm -rf ./build-$version || { echo 'Failed to delete /host/build-$version'; }
+mkdir ./build-$version || { echo 'Failed to create /host/build-$version'; }
+cd ./build-$version || { echo 'Failed to change to /host/build-$version directory'; exit 1; }
+../init-build.sh -DCAMKES_VM_APP=attarch-$version -DPLATFORM=qemu-arm-virt
+ninja
 
