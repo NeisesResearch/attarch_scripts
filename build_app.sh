0ffc5ed31f16111e97467bb57b98c1592d5aa4db
Michael Neises <neisesmichael@gmail.com>
2023-06-30 19:34:13 -0500

    ensure orthogonal build directories

--- build_app.sh
+++ build_app.sh
@@ -13,9 +13,9 @@ version=$1
 THIS_DIR=`pwd`
 cd seL4-CAmkES-L4v-dockerfiles &&
 make EXEC='bash -c "\
-    rm -rf /host/build || { echo '\''Failed to delete /host/build'\''; exit 1; }; \
-    mkdir /host/build || { echo '\''Failed to create /host/build'\''; exit 1; }; \
-    cd /host/build || { echo '\''Failed to change to /host/build directory'\''; exit 1; }; \
+    rm -rf /host/build-'$version' || { echo '\''Failed to delete /host/build-'$version''\''; exit 1; }; \
+    mkdir /host/build-'$version' || { echo '\''Failed to create /host/build-'$version''\''; exit 1; }; \
+    cd /host/build-'$version' || { echo '\''Failed to change to /host/build-'$version' directory'\''; exit 1; }; \
     ../init-build.sh -DCAMKES_VM_APP=attarch-'$version' -DPLATFORM=qemu-arm-virt && \
     ninja"' user HOST_DIR=$THIS_DIR
 
