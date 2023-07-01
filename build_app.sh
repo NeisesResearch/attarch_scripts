116b5ca0a49b9d73fb3b43d51dc22c115597212a
Michael Neises <neisesmichael@gmail.com>
2023-06-30 21:11:51 -0500

    Ensure build_dir exists

--- build_app.sh
+++ build_app.sh
@@ -13,7 +13,7 @@ version=$1
 THIS_DIR=`pwd`
 cd seL4-CAmkES-L4v-dockerfiles &&
 make EXEC='bash -c "\
-    rm -rf /host/build-'$version' || { echo '\''Failed to delete /host/build-'$version''\''; exit 1; }; \
+    rm -rf /host/build-'$version' || { echo '\''Failed to delete /host/build-'$version''\''; }; \
     mkdir /host/build-'$version' || { echo '\''Failed to create /host/build-'$version''\''; exit 1; }; \
     cd /host/build-'$version' || { echo '\''Failed to change to /host/build-'$version' directory'\''; exit 1; }; \
     ../init-build.sh -DCAMKES_VM_APP=attarch-'$version' -DPLATFORM=qemu-arm-virt && \
